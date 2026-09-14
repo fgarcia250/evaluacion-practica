2a9a3c4f41eb31b551f32e18d5248f701dbd9c38
Felix Garcia <gelixfarcia@gmail.com>
2026-09-14 17:01:01 -0600

    Includos cdk constructs para database, lambda y apigateway con su apiKey para control de acceso a los endpoints

--- lib/aws-evaluacion-practica-stack.ts
+++ lib/aws-evaluacion-practica-stack.ts
@@ -1,3 +1,6 @@
+import { ApiKey, ApiKeySourceType, RestApi, UsagePlan } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -6,6 +9,38 @@ export class AwsEvaluacionPracticaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    
+    const dbTable = new Table(this, 'DbTable', {
+      partitionKey: { name: 'pk', type: AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+
+    const api = new RestApi(this, 'RestAPI', {
+      restApiName: 'RestAPI',
+      apiKeySourceType: ApiKeySourceType.HEADER
+    });
+
+    const apiKey = new ApiKey(this, 'ApiKey');
+    const usagePlan = new UsagePlan(this, 'UsagePlan', {
+      name: 'UsagePlan',
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage
+        }
+      ]
+    });
+
+    usagePlan.addApiKey(apiKey);
+
+    const serviceLambda = new NodejsFunction(this, 'serviceLambda', {
+      entry: 'src/service.ts',
+      handler: 'handler',
+      environment: {
+        TABLE_NAME: dbTable.tableName
+      }
+    });
+
+    dbTable.grantReadWriteData(serviceLambda);
   }
 }
